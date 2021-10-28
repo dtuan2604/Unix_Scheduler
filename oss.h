21b4149a645f5094a370c5d9188cb8efdac3765d
Duong Hoang <hoang@hoare7.cs.umsl.edu>
2021-10-27 22:51:08 -0500

    Define data structure needed for oss to run

--- oss.h
+++ oss.h
@@ -2,8 +2,64 @@
 #define OSS_H
 
 #define MAXSECONDS 3
+#define processSize 18
+#define TIMESLICE 10000000
+//define key for shared memory
+const key_t key_shmem = 2604;
+const key_t key_queue = 1708;
 
+//define states for process
+enum state{
+	sNEW = 0,
+	sREADY,
+	sBLOCKED,
+	sTERMINATED
+};
 
+//define message format to communicate between scheduler and child process
+struct ossMsg{
+	long mtype;
+	pid_t from;
+	
+	int timeslice;
+	
+	struct timespec clock;
+	struct timespec io;
 
+};
+#define MESSAGE_SIZE (sizeof(struct ossMsg) - sizeof(long))
+
+//mark queue types: high-priority, low-priority, and blocked
+enum qTypes{
+	qHIGH = 0,
+	qLOW,
+	qBLOCKED,
+	qCOUNT
+};
+
+//define queue
+struct queue{
+	unsigned int ids[processSize];
+	unsigned int len;
+};
+
+//define process control block
+struct userPCB{
+	pid_t pid;
+	unsigned int id;
+	enum state state;
+
+	struct timespec t_cpu;
+	struct timespec t_sys;
+	struct timespec t_burst;
+	struct timespec t_blocked;
+	struct timespec t_started;
+};
+
+//define shared memory
+struct shmem{
+	struct timespec clock;
+	struct userPCB users[processSize];
+};
 
 #endif
